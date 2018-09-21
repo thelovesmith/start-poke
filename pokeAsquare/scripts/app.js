1b6308d4b209f3877c7a9b722b48cd7a0cb4ca84
thelovesmith <averydante9@gmail.com>
2018-09-21 11:26:50 -0500

    Commit 1 - made squares generate from button

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,1 +1,17 @@
+const $squares = $('.squares');
+const creatSquares = (numberOfSquares) => {
+	for (let i = 0; i < numberOfSquares; i++) {
+		$squares.clone().appendTo('.squares')
+	}
+}
 
+
+
+
+
+$('button').on('click', () => { 
+
+creatSquares(3)
+
+	console.log('click is on');
+});
