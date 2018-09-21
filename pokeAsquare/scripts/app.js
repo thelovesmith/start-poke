798340fa9a147a5a3bc7b451561199d15285cf58
thelovesmith <averydante9@gmail.com>
2018-09-21 12:02:06 -0500

    Commit 4 - Made squares generate ass random colors

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,28 +1,32 @@
 const $squares = $('.squares');
-// this is how I did everything //
-// const creatSquares = (numberOfSquares) => {
-// 	for (let i = 0; i < numberOfSquares; i++) {
-// 		$squares.clone().appendTo($squares)
-// 	}
-// }
+
+// let blue = 'blue'
+// let red = 'red'
+// let green = 'green'
 
 
 
+// This is how James did everything //
+const creatSquares = (numberOfSquares) => {
+	//create array to pick numbers from// 
+	const colorArr = ['blue', 'red', 'green', 'orange', 'violet', 'yellow']
+	//for loop to make divs based on numberOfSquares//
+	for (let i = 0; i < numberOfSquares; i++) {
+		//creating squares//
+		const $div = $('<div/>');
+
+		//Generate random color for each div//
+		$div.css('background-color', `${colorArr[Math.floor(Math.random() * 6)]}` );
+
+		//attach to the square class//
+		$squares.append($div);
+	}
+}
 
 
 $('button').on('click', () => { 
 
-creatSquares(30)
+creatSquares(100)
 //WHen the user clicks the button, it populates the squares
 	console.log('click is on');
 });
-
-// This is how James did everything //
-const creatSquares = (numberOfSquares) => {
-	for (let i = 0; i < numberOfSquares; i++) {
-		//creating squares//
-		const $div = $('<div/>');
-		//attach to the square class
-		$squares.append($div);
-	}
-}
