3f194d9b6f4d448009fd4cc317792e96316f3951
thelovesmith <averydante9@gmail.com>
2018-09-21 13:14:15 -0500

    Added Commit 7- Made score add and subract according to color picked

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -1,6 +1,9 @@
 const $squares = $('.squares');
-const $clickSquare = $('.clickSquare')
-
+const $clickSquare = $('.clickSquare');
+let score = 0;
+const $h1 = $('h1')
+let time = 10;
+let round = 1;
 // let blue = 'blue'
 // let red = 'red'
 // let green = 'green'
@@ -28,6 +31,7 @@ const creatSquares = (numberOfSquares) => {
 	  //add class to square
 	  $div.addClass('.clickSquare')
 	  //Trying to add event listener to clickSquare class then fadeTo//
+	  //my way didn't work//
 	// 	$('.clickSquare').on('click', () =>{
 	// 	$('.clickSquare').fadeTo('fast', 0.2);
 	// });
@@ -38,14 +42,34 @@ const creatSquares = (numberOfSquares) => {
 	}
 }
 
+//function to make squares disappear once they are clicked//
 const disappearSquares = (e) =>{
 	//e is short for event
 	//e.currentTarget gives you the div you are clicking on
-	$(e.currentTarget).css('opacity', 0);
+	const color = $(e.currentTarget).css('background-color')
+	checkValidPoke(color)
+	 $(e.currentTarget).fadeTo('fast', 0.1);
+	
 }
 	
- 
+//function to check color and add point accordingly//
+const checkValidPoke = (colorAsString) =>{
+	console.log(colorAsString);
+	//checking if color is blue 
+	//if color is blue add one point to score//
+	if(colorAsString === 'rgb(0, 0, 255)'){
+		//updating score//
+		score ++
+	} else{
+		//subtract one point if color is not blue//
+		score--
+	}
+	//Sending new score to h1 in html//
+	$h1.text('Scoreboard: ' + score)
+} 
 
+
+//event listener and handler for clicking Begin button// 
 $('button').on('click', () => { 
 creatSquares(100)
 //WHen the user clicks the button, it populates the squares
