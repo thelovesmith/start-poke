b0edb000dccfaa7a3f66cdef64237bea1ae1bf3c
thelovesmith <averydante9@gmail.com>
2018-09-21 12:10:00 -0500

    Commit 5 - Edited generateRandomColor function

--- pokeAsquare/scripts/app.js
+++ pokeAsquare/scripts/app.js
@@ -5,27 +5,32 @@ const $squares = $('.squares');
 // let green = 'green'
 
 
-
-// This is how James did everything //
-const creatSquares = (numberOfSquares) => {
+//function to generate colors randomly//
+const generateRandomColors = () =>{
 	//create array to pick numbers from// 
 	const colorArr = ['blue', 'red', 'green', 'orange', 'violet', 'yellow']
+	//Generate random color for each div//
+	const color = colorArr[Math.floor(Math.random() * 6)]
+	return color
+}
+
+
+// Generate squares//
+const creatSquares = (numberOfSquares) => {
+	
 	//for loop to make divs based on numberOfSquares//
 	for (let i = 0; i < numberOfSquares; i++) {
 		//creating squares//
 		const $div = $('<div/>');
 
-		//Generate random color for each div//
-		$div.css('background-color', `${colorArr[Math.floor(Math.random() * 6)]}` );
-
+	  $div.css('background-color', `${generateRandomColors()}`)
 		//attach to the square class//
-		$squares.append($div);
+		$squares.append($div)
 	}
 }
 
 
 $('button').on('click', () => { 
-
 creatSquares(100)
 //WHen the user clicks the button, it populates the squares
 	console.log('click is on');
